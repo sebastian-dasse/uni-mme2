f2616d12419106e8d1ec93f08d6323e31bf9c8b4
sebastian-dasse <s50602@beuth-hochschule.de>
2014-11-20 11:51:34 +0100

    Progress.

--- ue04/server.js
+++ ue04/server.js
@@ -1,15 +1,36 @@
+/**
+ * A simple REST server for the entity "stream".
+ *
+ * @author Sebastian Dass&eacute;
+ */
+
+'use strict';
+
+var streams = [{
+    type: 'stream',
+    data: 'foo'
+}, {
+    type: 'stream',
+    data: 'bar'
+}, {
+    type: 'stream',
+    data: 'baz'
+}];
+
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.json());
 
+
+/**
+ * Show welcome message. ** TODO ** replace with better routing with express router.
+ */
 app.get('/', function(req, res) {
     res.send('<h1>The amazing MME2-Server</h1>call "/player" for the video player <br> call "/api/v1/stream" for the REST service');
 });
-
-app.use('/player', express.static('public'));
-
 
 /**
  * Logs some parameters of every HTTP request.
@@ -18,7 +39,6 @@ app.use(function(req, res, next) {
     console.log('%s %s %s %s', req.method, req.path, req.body, req.route);
     next();
 });
-
 
 /**
  * Returns all streams.
@@ -34,9 +54,8 @@ app.get('/api/v1/stream', function(req, res) {
  */
 app.get('/api/v1/stream/:index', function(req, res) {
     var stream = streams[req.params.index];
-    // var stream = streamAt(req.params.index, 'GET');
-    if (!stream) {
-        res.status(404).send(new Error());
+    if (typeof stream === 'undefined') {
+        res.status(404).send(Error());
     } else {
         res.send(stream);
     }
@@ -48,10 +67,10 @@ app.get('/api/v1/stream/:index', function(req, res) {
 app.post('/api/v1/stream', function(req, res) {
     var newStream = req.body;
     if (!newStream) {
-        res.status(404).send(new Error());
+        res.status(404).send(Error());
     } else {
         streams.push(req.body);
-        res.send('POST ' + JSON.stringify(req.body));
+        res.status(201).send('New stream created at ' + (streams.length - 1) + '.');
     }
 });
 
@@ -59,7 +78,7 @@ app.post('/api/v1/stream', function(req, res) {
  * Throws an error.
  */
 app.post('/api/v1/stream/:index', function(req, res) {
-    res.status(404).send(new Error());
+    res.status(404).send(Error('Not allowed to post to a specified index.'));
 });
 
 /**
@@ -68,10 +87,10 @@ app.post('/api/v1/stream/:index', function(req, res) {
 app.put('/api/v1/stream', function(req, res) {
     var newStreams = req.body;
     if (Object.prototype.toString.call(newStreams) !== '[object Array]') {
-        res.status(404).send(new Error('not an ARRAY'));
+        res.status(404).send(Error('The request body has to be an array.'));
     } else {
         streams = newStreams;
-        res.send('PUT ' + JSON.stringify(newStreams));
+        res.send('The collection of streams was successfully overwritten.');
     }
 });
 
@@ -82,13 +101,11 @@ app.put('/api/v1/stream', function(req, res) {
  */
 app.put('/api/v1/stream/:index', function(req, res) {
     var index = req.params.index;
-    // var stream = streamAt(index, 'PUT');
-    if (streams[index] === 'undefined') {
-        res.status(404).send(new Error());
+    if (typeof streams[index] === 'undefined') {
+        res.status(404).send(Error());
     } else {
-        var newStream = req.body
-        streams[index] = newStream;
-        res.send('PUT to ' + index + ': ' + JSON.stringify(newStream));
+        streams[index] = req.body;
+        res.send('The stream at ' + index + ' was successfully overwritten.');
     }
 });
 
@@ -99,7 +116,7 @@ app.put('/api/v1/stream/:index', function(req, res) {
  */
 app.delete('/api/v1/stream', function(req, res) {
     streams = [];
-    res.send('DELETE of all streams');
+    res.send('All streams were successfully deleted.');
 });
 
 /**
@@ -107,16 +124,18 @@ app.delete('/api/v1/stream', function(req, res) {
  */
 app.delete('/api/v1/stream/:index', function(req, res) {
     var index = req.params.index;
-    // var stream = streamAt(index, 'DELETE');
-    if (streams[index] === 'undefined') {
-        res.status(404).send(new Error());
+    if (typeof streams[index] === 'undefined') {
+        res.status(404).send(Error());
     } else {
         streams.splice(index, 1);
-        res.send('DELETE from ' + index);
+        res.send('The stream at ' + index + ' was successfully deleted.');
     }
 });
 
 
+/**
+ * Start the server.
+ */
 var server = app.listen(8000, function() {
 
     var host = server.address().address;
@@ -126,39 +145,13 @@ var server = app.listen(8000, function() {
 });
 
 
-var streams = [{
-    type: 'stream',
-    name: 'foo',
-    quality: 'high'
-}, {
-    type: 'stream',
-    name: 'bar',
-    quality: 'medium'
-}, {
-    type: 'stream',
-    name: 'baz',
-    quality: 'low'
-}];
-
-// var error = {
-//     type: 'error',
-//     statusCode: 404,
-//     msg: 'Requested resource not found'
-// };
-
+/**
+ * Constructs a simple error object with an optional message.
+ */
 var Error = function(msg) {
-    var err = {};
-    err.type = 'error';
-    err.statusCode = 404;
-    err.msg = msg || 'Requested resource not found';
-    return err;
+    return {
+        type: 'error',
+        statusCode: 404,
+        msg: msg || 'Requested resource not found.'
+    };
 };
-
-// var streamAt = function(index, method) {
-//     var element = streams[index];
-//     if (element !== undefined) {
-//         return element;
-//     }
-//     console.log('error' + (method === undefined ? '' : ': could not ' + method + ' stream ' + index));
-//     return new Error();
-// }
